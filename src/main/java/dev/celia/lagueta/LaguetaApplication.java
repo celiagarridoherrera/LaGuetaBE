6e8a5d91a0623e6c0d4ab27ebab1f457dee51a49
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-15 12:37:13 +0200

    First commit

---
+++ src/main/java/dev/celia/lagueta/LaguetaApplication.java
@@ -0,0 +1,13 @@
+package dev.celia.lagueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaguetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaguetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/celia/lagueta/LaguetaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.celia.lagueta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaguetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
