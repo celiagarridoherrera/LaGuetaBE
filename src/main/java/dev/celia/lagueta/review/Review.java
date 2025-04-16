9f53275d1ab5e71f1762b0110f8c98e1f1a1db2e
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-16 02:36:57 +0200

    Review Model create - getters and setters

---
+++ src/main/java/dev/celia/lagueta/review/Review.java
@@ -0,0 +1,64 @@
+package dev.celia.lagueta.review;
+
+import java.time.LocalDateTime;
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String author;
+    private String content;
+    private int rating;
+
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
