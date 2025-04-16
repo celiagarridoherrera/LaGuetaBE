5a69198f3078e552d998c0be7ead53010cd44c42
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-16 03:21:01 +0200

    Create model Image

--- src/main/java/dev/celia/lagueta/image/Image.java
+++ src/main/java/dev/celia/lagueta/image/Image.java
@@ -1,5 +1,28 @@
 package dev.celia.lagueta.image;
 
 public class Image {
+    private String name;
+    private String path;
+
+    public Image(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
 }
