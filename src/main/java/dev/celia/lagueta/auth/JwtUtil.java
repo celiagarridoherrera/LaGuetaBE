486358c5add8306b844c388ee255aa3b0af06a74
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-16 14:39:29 +0200

    JwtUtil implement for tokens

---
+++ src/main/java/dev/celia/lagueta/auth/JwtUtil.java
@@ -0,0 +1,27 @@
+package dev.celia.lagueta.auth;
+
+import java.util.Date;
+import java.security.Key;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expiration = 86400000;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+}
